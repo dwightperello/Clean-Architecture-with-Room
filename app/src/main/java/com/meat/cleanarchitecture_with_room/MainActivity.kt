a01c90a915f98f8a925b0d873dab79e8e250565b
dwight <d.perello@outlook.com>
2022-04-21 17:47:54 +0800

    start of Project

---
+++ app/src/main/java/com/meat/cleanarchitecture_with_room/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.meat.cleanarchitecture_with_room
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
